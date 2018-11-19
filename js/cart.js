f744874c5948c41d8c7644bfc314ba3075497399
Sven Thoenelt <sventhoenelt@gmx.de>
2018-11-19 15:13:19 +0000

    Version20181119

--- js/cart.js
+++ js/cart.js
@@ -1,81 +1,125 @@
-var itemCount = 0;
-var priceTotal = 0.0;
+var itemCount = "";
+var priceTotal = "";
+var productId = "";
+var productName ="";
+var productPrice = "";
+var productQty = "";
 
-// Add Item to Cart
-$('.addItem').click(function(){
-    itemCount ++;    
+var shoppingCart = [];
+var newItem = {};
+
+
+function findProduct(productName) {
+    for (var i = 0, len = shoppingCart.length; i < len; i++) {
+        if (shoppingCart[i].productName === productName)
+            return i; 
+    }
+    return -1;
+}
+
+function addQty(productName) {
+    for (var i = 0, len = shoppingCart.length; i < len; i++) {
+        if (shoppingCart[i].productName == productName){
+            shoppingCart[i].productQty += 1;
+            shoppingCart[i].productSubtotal = shoppingCart[i].productQty * shoppingCart[i].productPrice;
+            return shoppingCart[i]; 
+        }
+    }
+    return null;
+}
+
+function removeQty(productName) {
+    for (var i = 0, len = shoppingCart.length; i < len; i++) {
+        if (shoppingCart[i].productName == productName){
+            shoppingCart[i].productQty -= 1;
+            shoppingCart[i].productSubtotal = shoppingCart[i].productQty * shoppingCart[i].productPrice;
+            return shoppingCart[i]; 
+        }
+    }
+    return null;
+}
+
+function printCart()
+{
+    $('#cartItems').empty();
+    
+    itemCount = 0;
+    priceTotal = 0;
+    
+    for (var i = 0; i < shoppingCart.length; i++) {
+        itemCount += shoppingCart[i].productQty;
+        priceTotal += (shoppingCart[i].productPrice * shoppingCart[i].productQty);
+    }
     
     // Update cart count for large and small screens
     $('#itemCount').text(itemCount);
     $('#itemCountSm').text(itemCount);
     
-    // Add item name and price to cart and append trash can
-    $(this).siblings(".itemName")
-        .clone()
-        .appendTo('#cartItems')
-        .prepend('<i class="fas fa-trash text-danger pr-2 removeItem"></i>')
-        .append('<br>'); 
+    for (var i = 0; i < shoppingCart.length; i++) {
+        $('#cartItems').append(
+            '<tr>'+
+            '<td>'+'<i class="fas fa-trash text-danger pr-2 removeItem"></i>'+'</td>'+
+            '<td>'+shoppingCart[i].productName+'</td>'+
+            '<td>'+shoppingCart[i].productPrice+'</td>'+
+            '<td>'+shoppingCart[i].productQty+'</td>'+
+            '<td class="text-right">'+shoppingCart[i].productSubtotal+'</td>'+
+            '</tr>');
+    }
 
-    // Calculate Total Price
-    var price = parseFloat(
-        $(this).siblings().find('.price')
-        .text()
-        .replace(/[^0-9.-]+/g,"")
-    ); 
-    
-    console.log('price added:'+price);
-    
-    // Add item price to total price
-    priceTotal += price;
-    
     // Update total price text with new total price
-    $('#cartTotal').text("Total: $" + priceTotal.toFixed(2)).prepend('<hr>');
+    $('#cartTotal').text("Total: $" + priceTotal.toFixed(2));
+    
+    if(itemCount==0){
+        $('#cartTotal').text('');
+    }
+}
+
+// Add Item to Cart
+$('.addItem').click(function(){
+    itemCount ++;
+    
+    productName = $(this).siblings('.itemName').clone().children().remove().end().text();
+    productPrice = parseFloat($(this).siblings().find('.price').text().replace(/[^0-9.-]+/g,""));
+    
+    if(findProduct(productName) != -1){
+        addQty(productName)
+    }
+    else{
+        newItem = {
+            'productName': productName,
+            'productPrice': productPrice,
+            'productQty': 1,
+            'productSubtotal': productPrice
+        }
+
+        shoppingCart.push(newItem);
+    }
+    
+    printCart(); 
 });
 
 // Remove Item from Cart
 $('#shoppingCart').on('click', '.removeItem', function(){
     
-    // Parse item price and strip non-numbers
-    var price = parseFloat(
-        $(this).parent().find('.price')
-        .text()
-        .replace(/[^0-9.-]+/g,"")
-    );
+    productName = $(this).closest('tr').find('td:eq(1)').text();
+    productPrice = $(this).closest('tr').find('td:eq(2)').text();
+    productQty = $(this).closest('tr').find('td:eq(3)').text();
     
-    console.log('price removed:'+price);
-    
-    // Remove item price from total price
-    priceTotal -= price;
-    
-    // Update total price with new total price
-    $('#cartTotal').text("Total: $" + priceTotal.toFixed(2)).prepend('<hr>');
-
-    // Remove item from cart
-    $(this).parent().remove();
-  
-    itemCount --;
-    
-    // Update cart count for large and small screens
-    $('#itemCount').text(itemCount);
-    $('#itemCountSm').text(itemCount);
-    
-    // If cart count equals zero, hide total price
-    if (itemCount == 0) {
-        $('#cartTotal').text("");
+    if(productQty == 1){
+        productLoc = findProduct(productName);
+        shoppingCart.splice(productLoc,1);
     }
-
+    else{
+        removeQty(productName);
+    }
+    
+    printCart();
 });
 
 // Clear Cart
 $('#clearCart').click(function() {
+    shoppingCart = [];
     
-    // Set item count and total price to zero
-    itemCount = 0;
-    priceTotal = 0.0;
-
     // Clear appropriate text and update values
-    $('#cartItems').text('');
-    $('#itemCount').text(itemCount);
-    $('#itemCountSm').text(itemCount);
-    $('#cartTotal').text('');
+    printCart();
 }); 
